c61e5b0a983bf9d026e4fd79946952ce403c2489
Paing Soe Khant <babyboss7916@gmail.com>
2024-08-16 22:46:17 +0630

    project setup & bind bootstrap to project

---
+++ src/App.vue
@@ -0,0 +1,10 @@
+<template>
+  <div>
+    <HomePage/>
+  </div>
+</template>
+
+<script setup>
+
+  import HomePage  from "./views/HomePage.vue"
+</script>
---
+++ src/views/HomePage.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="container mt-3">
+        <h3 class="text-success">Hello World</h3>
+    </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style lang="scss" scoped>
+
+</style>
