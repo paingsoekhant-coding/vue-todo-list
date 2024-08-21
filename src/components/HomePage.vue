b2be3f29fdfe4c7844684b3a417730f8b502bbb4
Paing Soe Khant <babyboss7916@gmail.com>
2024-08-21 11:35:15 +0630

    [Complete]Filter todo

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -5,23 +5,38 @@
         <h4>Todo App With Vue</h4>
         <a href="" class="btn btn-dark text-white rounded">Add Todo</a>
       </div>
-      <div class="card-body ">
-        <div class="d-flex " v-for="(task, index) in tasks" :key="index">
-          <div class="d-flex col-8 ">
+      <div class="card-body">
+        <div class="text-center">
+          <label for="">Hide Complete Tasks</label>
+          <input
+            type="checkbox"
+            v-model="hideComplete"
+            class="form-check-input bg-secondary ms-2"
+          />
+        </div>
+        <div class="d-flex" v-for="(task, index) in filterTasks" :key="index">
+          <div class="d-flex col-8">
             <input
               class="form-check-input me-2 bg-secondary"
               type="checkbox"
               value=""
+              v-model="task.isDone"
             />
-            <p>{{ task.action }}</p>
+            <p v-bind:class="task.isDone ? 'done-tasks' : ' '">
+              {{ task.action }}
+            </p>
           </div>
-          <div class="col-4 d-flex justify-content-center ">
-            <p><a href="" class="btn btn-sm me-2 delete-btn text-white"
+          <div class="col-4 d-flex justify-content-center">
+            <p>
+              <a href="" class="btn btn-sm me-2 delete-btn text-white"
                 ><i class="fa-solid fa-trash"></i
-              ></a></p>
-            <p><a href="" class="btn btn-sm edit-btn text-white"
+              ></a>
+            </p>
+            <p>
+              <a href="" class="btn btn-sm edit-btn text-white"
                 ><i class="fa-solid fa-pen"></i
-              ></a></p>
+              ></a>
+            </p>
           </div>
         </div>
       </div>
@@ -33,6 +48,7 @@
 export default {
   name: "HomePage",
   data: () => ({
+    hideComplete: false,
     tasks: [
       { action: "Buy new phone", isDone: false },
       { action: "Buy food ", isDone: false },
@@ -40,6 +56,23 @@ export default {
       { action: "Buy shoes", isDone: false },
     ],
   }),
+  computed: {
+    // ES6 function
+    filterTasks() {
+      return this.hideComplete
+        ? this.tasks.filter((v) => !v.isDone)
+        : this.tasks;
+    },
+
+    //Normal Function
+    // filterTasks() {
+    //   if (this.hideComplete) {
+    //     return this.tasks.filter((v) => !v.isDone);
+    //   } else {
+    //     return this.tasks;
+    //   }
+    // },
+  },
 };
 </script>
 
@@ -57,5 +90,9 @@ export default {
 
 .card-body {
   background-color: cornsilk;
+}
+
+.done-tasks {
+  text-decoration: line-through;
 }
 </style>
